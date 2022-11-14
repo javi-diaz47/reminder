ac3522ceec4143a9b579bea0acfdb80b60351e72
javi-diaz47 <javiereduardo300@gmail.com>
2022-11-14 11:07:37 -0500

    style

--- pages/reminder/[id].jsx
+++ pages/reminder/[id].jsx
@@ -4,12 +4,13 @@ import { getUserIdFromToken } from "../../utils/getUserIdFromToken";
 import { supabase } from "../../utils/supabase";
 
 export default function ReminderId({ medicine }) {
-  console.log(medicine);
+  const onSubmit = () => {};
+
   return (
     <div>
       <h2 className="text-3xl capitalize font-bold">Crear recordatorio</h2>
-      <p className="text-2xl">{medicine.name}</p>
-      <form className="flex flex-col gap-8">
+      <p className="text-2xl mb-10">{medicine.name}</p>
+      <form onSubmit={onSubmit} className="flex flex-col gap-8">
         <div>
           <label className="text-lg">Hora</label>
           <div className="flex gap-8">
@@ -19,15 +20,19 @@ export default function ReminderId({ medicine }) {
         </div>
         <div>
           <label className="text-lg">Frecuencia</label>
-          <select
-            id="frequency"
-            className="block py-2.5 px-0 w-full  text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
-          >
-            <option selected>Elige una frecuencia</option>
-            <option value="daily">Diario</option>
-            <option value="0/2">Cada 2 dias</option> {/* 0 0 * * 0/2 */}
-            <option value="0/3">Cada 3 dias</option>
-          </select>
+          <div className="flex items-center gap-4">
+            Cada
+            <Input placeholder="6" name="frequency-number" type="number" step="1" min="0" max="31" />
+            <select
+              name="frequency-text"
+              className="block py-2.5 px-0 w-full  text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+            >
+              <option selected="hourly">horas</option>
+              <option value="daily">dias</option>
+              {/* <option value="0/2">Cada 2 dias</option> 0 0 * * 0/2 */}
+              {/* <option value="0/3">Cada 3 dias</option> */}
+            </select>
+          </div>
         </div>
         <div className="flex justify-center">
           <Button type="submit" value="Crear" />
@@ -50,6 +55,7 @@ export async function getServerSideProps(context) {
   console.log(medicine);
   return {
     props: {
+      userId: id,
       medicine,
     },
   };
