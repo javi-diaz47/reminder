099de2d9374695d497d14f8b9f2b3783871899f4
javi-diaz47 <javiereduardo300@gmail.com>
2022-11-13 23:52:23 -0500

    feat: add reminder form

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button(props) {
   return (
     <button
-      className="bg-primary py-2 px-4 rounded-xl text-white font-bold hover:bg-white hover:text-primary transition-all duration-300 shadow-lg"
+      className="text-lg bg-primary py-2 px-4 rounded-xl text-white font-bold hover:bg-white hover:text-primary transition-all duration-300 shadow-lg"
       {...props}
     >
       {props.value}
--- pages/reminder/[id].jsx
+++ pages/reminder/[id].jsx
@@ -1,3 +1,5 @@
+import Button from "../../components/Button";
+import { Input } from "../../components/Input";
 import { getUserIdFromToken } from "../../utils/getUserIdFromToken";
 import { supabase } from "../../utils/supabase";
 
@@ -5,7 +7,32 @@ export default function ReminderId({ medicine }) {
   console.log(medicine);
   return (
     <div>
-      <h2 className="text-3xl capitalize">{medicine.name}</h2>
+      <h2 className="text-3xl capitalize font-bold">Crear recordatorio</h2>
+      <p className="text-2xl">{medicine.name}</p>
+      <form className="flex flex-col gap-8">
+        <div>
+          <label className="text-lg">Hora</label>
+          <div className="flex gap-8">
+            <Input placeholder="Hora" name="hour" type="number" step="1" min="0" max="23" />
+            <Input placeholder="Minutos" name="mintue" type="number" step="1" min="0" max="59" />
+          </div>
+        </div>
+        <div>
+          <label className="text-lg">Frecuencia</label>
+          <select
+            id="frequency"
+            className="block py-2.5 px-0 w-full  text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+          >
+            <option selected>Elige una frecuencia</option>
+            <option value="daily">Diario</option>
+            <option value="0/2">Cada 2 dias</option> {/* 0 0 * * 0/2 */}
+            <option value="0/3">Cada 3 dias</option>
+          </select>
+        </div>
+        <div className="flex justify-center">
+          <Button type="submit" value="Crear" />
+        </div>
+      </form>
     </div>
   );
 }
