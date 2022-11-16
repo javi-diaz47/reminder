60ce6afd90f81ede755fea9c9e6ba187a9c7b65b
javi-diaz47 <javiereduardo300@gmail.com>
2022-11-15 22:42:36 -0500

    feat: add login styles

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,5 +1,8 @@
+import Image from "next/image";
 import { useState } from "react";
 import { signInWithEmail, signInWithGoogle } from "../../utils/auth";
+import googleIcon from "../../public/google-icon.svg";
+import loginSVG from "../../public/login.svg";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -11,12 +14,19 @@ function Login() {
     }
   };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input name="email" value={email} />
-        <button type="submit">Ingresar</button>
-      </form>
-      <button onClick={signInWithGoogle}>Continue with Google</button>
+    <div className="flex justify-center items-center flex-col text-center ">
+      <Image src={loginSVG} alt="login image" />
+      <h2 className="text-2xl mb-8">
+        Crea una cuenta o Inicia sesión
+        <p className="font-bold">¡Todo con un solo click!</p>
+      </h2>
+      <button
+        className="w-fit rounded-2xl shadow-xl flex flex-col justify-center items-center p-4 hover:scale-105 active:scale-95 duration-300 transition-all"
+        onClick={signInWithGoogle}
+      >
+        <Image src={googleIcon} alt={"Google icon"} />
+        Continuar con google
+      </button>
     </div>
   );
 }
