0e67c51684895a9eea120e4bb38fe7d62e7c92df
javi-diaz47 <javiereduardo300@gmail.com>
2022-11-15 21:09:02 -0500

    adding images

--- pages/add-medicine/index.jsx
+++ pages/add-medicine/index.jsx
@@ -2,7 +2,7 @@ import { getCookie } from "cookies-next";
 import { AnimatePresence, MotionConfig } from "framer-motion";
 import jwt from "jsonwebtoken";
 import { useState } from "react";
-import Button from "../../components/Button";
+import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { addMedicineDB } from "../../utils/addMedicine";
 import { supabase } from "../../utils/supabase";
